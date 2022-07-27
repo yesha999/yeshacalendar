f22a7dcd4bb0dba9aaf83c09dc0319e41ca1cb65
yesha999 <scrolltrip@mail.ru>
2022-07-27 19:29:45 +0300

    Добавлены докер файлы. Реализован action для пуша и деплоя приложения

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
 
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-*^4$q$pne1(y2ijgzb@q(vayp=ia036o%a_emv-*2$9v@69gby"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
