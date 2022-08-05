d1f10f3327948024dfc364765c252427a1430ee5
yesha999 <scrolltrip@mail.ru>
2022-08-05 11:38:34 +0300

    .

--- todolist/settings.py
+++ todolist/settings.py
@@ -24,7 +24,7 @@ load_dotenv(dotenv_path=env_path)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = f"{os.getenv('SECRET_KEY')}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
