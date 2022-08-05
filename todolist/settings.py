0a0e56054a55d093fa540fb413a68431065cf84f
yesha999 <scrolltrip@mail.ru>
2022-08-05 12:16:07 +0300

    добавлен SOCIAL_AUTH_PIPELINE

--- todolist/settings.py
+++ todolist/settings.py
@@ -131,7 +131,18 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     # бекенд классической аутентификации, чтобы работала авторизация через обычный логин и пароль
 )
-
+SOCIAL_AUTH_URL_NAMESPACE = "social"
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
 SOCIAL_AUTH_VK_OAUTH2_KEY = 51394349
 SOCIAL_AUTH_VK_OAUTH2_SECRET = f"{os.getenv('SOCIAL_AUTH_VK_OAUTH2_SECRET')}"
 SOCIAL_AUTH_VK_OAUTH2_SCOPE = ["email", "photos", "notify"]
