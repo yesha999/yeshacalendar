a742a9c715d85f491dac298ea762a3ff7427e0a7
yesha999 <scrolltrip@mail.ru>
2022-08-10 18:07:00 +0300

    Удалена возможность изменения цели при редактировании комментария к ней

--- goals/serializers.py
+++ goals/serializers.py
@@ -57,4 +57,4 @@ class GoalCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = GoalComment
         fields = "__all__"
-        read_only_fields = ("id", "created", "updated", "user")
+        read_only_fields = ("id", "created", "updated", "user", "goal")
