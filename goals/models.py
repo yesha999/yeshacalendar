a1770f82ffd68dd99e173794f8cf8e6432b0f530
yesha999 <scrolltrip@mail.ru>
2022-08-15 15:50:29 +0300

    созданы  модели для новой сущности "Доска" и для "Участника доски"

--- goals/models.py
+++ goals/models.py
@@ -18,11 +18,51 @@ class DatesModelMixin(models.Model):
         return super().save(*args, **kwargs)
 
 
+class Board(DatesModelMixin):
+    class Meta:
+        verbose_name = "Доска"
+        verbose_name_plural = "Доски"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+
+class BoardParticipant(DatesModelMixin):
+    class Meta:
+        unique_together = ["board", "user"]
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
+
+    class Role(models.IntegerChoices):
+        owner = 1, "Владелец"
+        writer = 2, "Редактор"
+        reader = 3, "Читатель"
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name="Роль", choices=Role.choices, default=Role.owner
+    )
+
+
 class GoalCategory(DatesModelMixin):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
+    board = models.ForeignKey(
+        Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories"
+    )
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
@@ -63,6 +103,3 @@ class GoalComment(DatesModelMixin):
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     goal = models.ForeignKey(Goal, verbose_name="Цель", on_delete=models.PROTECT)
     text = models.TextField(verbose_name="Текст комментария")
-
-
-
