b36af9f1dcce4968c032c8831f4f66f37acefd8b
yesha999 <scrolltrip@mail.ru>
2022-08-31 18:43:32 +0300

    Оптимизировано для использования нескольких пользователей

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -21,35 +21,35 @@ class Command(BaseCommand):
         else:
             self.tg_client.send_message(message.chat.id, "Нет целей, /create для создания")
 
-    def create_goal(self, message: Message, board_title, category_title):
-        tg_user = TgUser.objects.get(tg_user_id=message.from_.id)
-        category = GoalCategory.objects.filter(board__title=board_title, title=category_title, user=tg_user.user,
-                                               board__participants__role__in=[BoardParticipant.Role.owner,
-                                                                              BoardParticipant.Role.writer]).first()
-        goal = Goal.objects.create(title=message.text, user=tg_user.user, category=category)
+    def create_goal(self, message: Message, tg_user: TgUser):
+        goal = Goal.objects.create(title=message.text, user=tg_user.user, category=tg_user.active_category)
         self.tg_client.send_message(
             message.chat.id, f"Цель {goal.title} создана!,"
                              f" подробнее: http://yeshacalendar.ga/categories/goals?goal={goal.id}")
 
-    def send_goal_name_question(self, message: Message, board_title):
-        tg_user = TgUser.objects.get(tg_user_id=message.from_.id)
-        board = Board.objects.filter(is_deleted=False, title=board_title, participants__user=tg_user.user).first()
-        category = GoalCategory.objects.filter(board=board, title=message.text, user=tg_user.user).first()
+    def send_goal_name_question(self, message: Message, tg_user: TgUser):
+        category = GoalCategory.objects.filter(
+            board=tg_user.active_board, title=message.text, user=tg_user.user,
+            board__participants__role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer]).first()
         if not category:
             category = GoalCategory.objects.create(
-                board=board, title=message.text, user=tg_user.user)
-
+                board=tg_user.active_board, title=message.text, user=tg_user.user)
+        tg_user.active_category = category
+        tg_user.save(update_fields=["active_category"])
         self.tg_client.send_message(
-            message.chat.id, f"Выбрана доска {board_title}, категория {category.title}, укажите название цели")
+            message.chat.id, f"Выбрана доска {tg_user.active_board.title}, категория {category.title},"
+                             f" укажите название цели")
 
-    def send_categories(self, message: Message):
-        tg_user = TgUser.objects.get(tg_user_id=message.from_.id)
+    def send_categories(self, message: Message, tg_user: TgUser):
         board = Board.objects.filter(title=message.text, participants__user=tg_user.user, is_deleted=False).first()
         if not board:
             board = Board.objects.create(title=message.text)  # Создали или получили доску
             BoardParticipant.objects.create(user=tg_user.user, board=board, role=BoardParticipant.Role.owner)
+        tg_user.active_board = board
+        tg_user.save(update_fields=["active_board"])
         categories = GoalCategory.objects.filter(board=board, is_deleted=False)
-        self.tg_client.send_message(message.chat.id, "Напишите название категории (если такой нет, будет создана новая)")
+        self.tg_client.send_message(message.chat.id,
+                                    "Напишите название категории (если такой нет, будет создана новая)")
         if categories:
             self.tg_client.send_message(message.chat.id,
                                         '\n'.join([f"{category.id}) {category.title}" for category in categories]))
@@ -65,14 +65,28 @@ class Command(BaseCommand):
             self.tg_client.send_message(message.chat.id, "Существующих досок не найдено")
 
     def handle_user_message(self, message: Message, tg_user: TgUser):
-        if "/goals" in message.text:
-            self.send_goals(message, tg_user)
-        elif "/create" in message.text:
-            self.send_boards(message, tg_user)
-        elif "/cancel" in message.text:
+        if tg_user.bot_condition == TgUser.Condition.start:
+            if "/goals" in message.text:
+                self.send_goals(message, tg_user)
+            elif "/create" in message.text:
+                tg_user.bot_condition = TgUser.Condition.create1
+                self.send_boards(message, tg_user)
+            else:
+                self.tg_client.send_message(message.chat.id, "Неизвестная команда")
+        elif tg_user.bot_condition != TgUser.Condition.start and "/cancel" in message.text:
+            tg_user.bot_condition = TgUser.Condition.start
             self.tg_client.send_message(message.chat.id, "Вернулись в начало")
-        else:
-            self.tg_client.send_message(message.chat.id, "Неизвестная команда")
+        elif tg_user.bot_condition == TgUser.Condition.create1:
+            self.send_categories(message, tg_user)
+            tg_user.bot_condition = TgUser.Condition.create2  # Состояние ожидания названия категории
+        elif tg_user.bot_condition == TgUser.Condition.create2:
+            self.send_goal_name_question(message, tg_user)
+            tg_user.bot_condition = TgUser.Condition.create3  # Состояние ожидания названия цели
+        elif tg_user.bot_condition == TgUser.Condition.create3:
+            self.create_goal(message, tg_user)
+            tg_user.bot_condition = TgUser.Condition.start
+        tg_user.save(update_fields=["bot_condition"])
+
 
     def handle_verification_message(self, message: Message, tg_user: TgUser):
         tg_user.set_verification_code()
@@ -91,35 +105,9 @@ class Command(BaseCommand):
             self.handle_user_message(message, tg_user)
 
     def handle(self, *args, **options):
-        condition = "start"
         offset = 0
-        board_title = "default_title"
-        category_title = "default_title"
         while True:
             res = self.tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
-                if item.message.text == "/create" \
-                        and item.message.from_.username in \
-                        [user.tg_username for user in TgUser.objects.all()]:  # Нужно для безошибочной работы,
-                    # если неверифицированный пользователь написал /create
-                    condition = "create1"  # состояние ожидания названия доски
-                    self.handle_message(item.message)
-                    continue
-                if item.message.text == "/cancel":
-                    condition = "start"
-                if condition == "start":
-                    self.handle_message(item.message)
-                elif condition == "create1":  # Пользователь отправляет название доски
-                    self.send_categories(item.message)
-                    board_title = item.message.text  # запомнить для создания категории
-                    condition = "create2"  # Состояние ожидания названия категории
-                    continue
-                elif condition == "create2":  # Пользователь отправляет название категории
-                    self.send_goal_name_question(item.message, board_title)
-                    category_title = item.message.text  # запомнить для создания цели
-                    condition = "create3"  # Состояние ожидания названия цели
-                    continue
-                elif condition == "create3":
-                    self.create_goal(item.message, board_title, category_title)
-                    condition = "start"
+                self.handle_message(item.message)
--- bot/models.py
+++ bot/models.py
@@ -3,6 +3,7 @@ import random
 from django.db import models
 
 from core.models import User
+from goals.models import Board, GoalCategory
 
 SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM12345678901234567890"
 
@@ -11,6 +12,12 @@ class TgUser(models.Model):
     class Meta:
         verbose_name = "Телеграм Юзер"
         verbose_name_plural = "Телеграм Юзеры"
+
+    class Condition(models.IntegerChoices):
+        create1 = 1, "Ожидание названия доски"
+        create2 = 2, "Ожидание названия категории"
+        create3 = 3, "Ожидание названия цели"
+        start = 4, "Начало"
 
     tg_chat_id = models.BigIntegerField(verbose_name="Телеграм чат id")
     tg_user_id = models.BigIntegerField(verbose_name="Телеграм юзер id", unique=True)
@@ -21,6 +28,14 @@ class TgUser(models.Model):
                              blank=True, default=None)
     verification_code = models.CharField(max_length=5, null=True, blank=True, default=None,
                                          verbose_name="Код подтверждения")
+    bot_condition = models.PositiveSmallIntegerField(verbose_name="Состояние бота", choices=Condition.choices,
+                                                     default=Condition.start)
+
+    active_board = models.ForeignKey(Board, verbose_name="Поле доски для создания категории", on_delete=models.PROTECT,
+                                     null=True, blank=True, default=None) #  поле нужно для того,
+    # чтобы запомнить в процессе исполнения бота в какую доску класть создаваемую цель
+    active_category = models.ForeignKey(GoalCategory, verbose_name="Поле категории для создания цели",
+                                        on_delete=models.PROTECT, null=True, blank=True, default=None)
 
     def set_verification_code(self):
         self.verification_code = "".join([random.choice(SYMBOLS) for i in range(5)])
